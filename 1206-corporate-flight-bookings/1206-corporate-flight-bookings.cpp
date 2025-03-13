ac3bf8e3a095de7f45d772e7b887045a2e1e6df1
Devansh Shah <shahdevansh847@gmail.com>
2025-03-13 22:05:19 +0530

    Time: 1 ms (73.53%), Space: 72.2 MB (54.2%) - LeetHub

---
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> diff(n, 0);
+        for(int i=0; i<bookings.size(); i++){
+            int start=bookings[i][0];
+            int end=bookings[i][1];
+            int delta=bookings[i][2];
+            
+            diff[start-1]+=delta;
+            if(end<n) diff[end]-=delta;
+        }
+        int cumsum=0;
+        for(int i=0; i<n; i++){
+            cumsum+=diff[i];
+            diff[i]=cumsum;
+        }
+        return diff;
+    }
+};

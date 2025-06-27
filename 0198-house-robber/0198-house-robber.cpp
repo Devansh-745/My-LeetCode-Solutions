cf0db6df4492b1a9dfcce7292b98bf82a1ea228d
Devansh Shah <shahdevansh847@gmail.com>
2025-06-27 18:34:34 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.73%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int memoiz(vector<int>& nums, int i, vector<int>& dp, int n){
+        if(i==n-1) return nums[n-1];
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int pick=nums[i]+memoiz(nums, i+2, dp, n);
+        int notpick=memoiz(nums, i+1, dp, n);
+
+        return dp[i]=max(pick, notpick);
+    }
+
+    int rob(vector<int>& nums) {
+        int n= nums.size();
+        vector<int>dp(n+1, -1);
+        return memoiz(nums, 0, dp, n);
+    }
+};

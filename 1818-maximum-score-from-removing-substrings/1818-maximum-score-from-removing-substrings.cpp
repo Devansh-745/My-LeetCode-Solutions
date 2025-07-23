297a33c8bb0f5adfab054746c15dc8ba104aa4f9
Devansh Shah <shahdevansh847@gmail.com>
2025-07-23 17:57:14 +0530

    Time: 32 ms (73.45%), Space: 24.7 MB (73.02%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    int pointsfinder(string &s, string target, int gain){
+    // int pointsfinder(string &s, string target, int gain){
+    //     int points=0;
+    //     stack<char> st;
+    //     for(int i=0; i<s.length(); i++){
+    //         char cur=s[i];
+    //         if(!st.empty() && st.top()==target[0] && cur==target[1]){
+    //             st.pop();
+    //             points+=gain;
+    //         }
+    //         else{
+    //             st.push(cur);
+    //         }
+    //     }
+    //     //reconstruct remaining string;
+    //     string temp="";
+    //     while(!st.empty()){
+    //         temp+=st.top();
+    //         st.pop();
+    //     }
+    //     reverse(temp.begin(), temp.end());
+    //     s=temp;
+    //     return points;
+    // }
+    int pointsfinderII(string &s, string target, int gain){
+        int i=0;
+        int j=0;
+        string temp=s;
+        int n=s.length();
         int points=0;
-        stack<char> st;
-        for(int i=0; i<s.length(); i++){
-            char cur=s[i];
-            if(!st.empty() && st.top()==target[0] && cur==target[1]){
-                st.pop();
+        while(j<n){
+            temp[i]=temp[j];
+            i++;
+            if(i-1>=0 && i-2>=0 && temp[i-1]==target[1] && temp[i-2]==target[0]){
                 points+=gain;
-            }
-            else{
-                st.push(cur);
-            }
+                i-=2;
+            } 
+            j++;
         }
-        //reconstruct remaining string;
-        string temp="";
-        while(!st.empty()){
-            temp+=st.top();
-            st.pop();
-        }
-        reverse(temp.begin(), temp.end());
+        temp.erase(temp.begin()+i, temp.end());
         s=temp;
         return points;
     }
@@ -29,12 +48,12 @@ public:
         string first="ab";
         string second="ba";
         if(x>y){
-            ans+=pointsfinder(s, first, x);
-            ans+=pointsfinder(s, second, y);
+            ans+=pointsfinderII(s, first, x);
+            ans+=pointsfinderII(s, second, y);
         }
         else{
-            ans+=pointsfinder(s, second, y);
-            ans+=pointsfinder(s, first, x);
+            ans+=pointsfinderII(s, second, y);
+            ans+=pointsfinderII(s, first, x);
         }
         return ans;
     }

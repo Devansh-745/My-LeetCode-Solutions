35cd149f6ba4851197194dd0065b13cf3b886d02
Devansh Shah <shahdevansh847@gmail.com>
2025-08-15 15:34:19 +0530

    Time: 2 ms (67.49%), Space: 9.2 MB (7.07%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=0) return false;
+        int x=round(log(n)/log(3));
+        if(pow(3, x)==n) return true;
+        return false;
+    }
+};
